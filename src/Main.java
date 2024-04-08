304cc5681cba100aae5ca32dc66cdb7c45e39479
TMolak <tomekmolak@gmail.com>
2024-04-08 17:09:15 +0200

    working perceptron

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,34 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Proszę podaj ścieżkę do pliku treningowego:");
-        trainingPath = scanner.nextLine();
-
+//        trainingPath = scanner.nextLine();
+trainingPath = "D:\\Dokumnety\\Studia\\IV semestr\\NAI\\Mpp\\NAI - perceptron\\data\\iris_perceptron\\training.txt";
         System.out.println("Proszę podaj ścieżkę do pliku testowego:");
-        testingPath = scanner.nextLine();
-
-
+//        testingPath = scanner.nextLine();
+testingPath = "D:\\Dokumnety\\Studia\\IV semestr\\NAI\\Mpp\\NAI - perceptron\\data\\iris_perceptron\\test.txt";
         System.out.println("Podaj wartość stałej uczenia:");
         learningRate = scanner.nextDouble();
 
 
         System.out.println("Podaj liczbę epok:");
-        epochs = scanner.nextInt();
-        System.out.println("Nieprawidłowa wartość. Podaj liczbę całkowitą.");
-        scanner.next();
 
+        epochs = scanner.nextInt();
+
+
+        Perceptron perceptron = new Perceptron(learningRate, trainingPath, testingPath, epochs);
+
+        perceptron.train(perceptron.trainingList);
+
+        int correct = 0;
+        for (Observation observation : perceptron.testList) {
+            String prediction = perceptron.predict(observation);
+            System.out.println("Dla obserwacji: " + observation + " sklasyfikowano: " + prediction);
+            if (observation.getLabel().equals(String.valueOf(prediction))) {
+                correct++;
+            }
+        }
+        double accuracy = (double) correct / perceptron.testList.size() * 100;
+        System.out.println("Test zakończony. Dokładność perceptronu: " + accuracy + "%.");
 
     }
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,66 +1,107 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Perceptron {
 
     private double[] weights;
-    private double theta;
-    private double learningConstant;
+    private double bias;
+    private double learningRate;
 
-    public List<Observation> observationList;
+    private int epoch;
+    public List<Observation> trainingList;
+    public List<Observation> testList;
 
-    public Perceptron(double learningConstant, String path) {
-        loadData(path);
-        this.learningConstant = learningConstant;
-        this.weights = new double[observationList.size()];
-        weigthInitializacion();
+    private Map<String, Integer> labelMap = new HashMap<>();
+    private String positiveLabel, negativeLabel;
+
+
+    public Perceptron(double learningRate, String trainingPath, String testPath, int epoch) {
+        this.learningRate = learningRate;
+        trainingList = loadData(trainingPath);
+        testList = loadData(testPath);
+        this.epoch = epoch;
+        initializeWeights(trainingList.get(0).getFeatures().length);
     }
 
-    public void loadData(String path){
+    public List<Observation> loadData(String path){
+        List<Observation> list = new ArrayList<>();
+        int labelIndex = 0;
+
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
             String line;
             while((line = br.readLine()) != null){
-                String[] spliitedLine = line.split(",");
-                double[] features = new double[spliitedLine.length-1];
+                String[] splitedLine = line.split(",");
+                double[] features = new double[splitedLine.length-1];
                 for (int i = 0; i < features.length; i++) {
-                    features[i] = Double.parseDouble(spliitedLine[i]);
+                    features[i] = Double.parseDouble(splitedLine[i]);
                 }
-                String label = spliitedLine[spliitedLine.length-1];
+                String label = splitedLine[splitedLine.length-1];
+
                 Observation observation = new Observation(label, features);
-                observationList.add(observation);
+                list.add(observation);
+
+                if (!labelMap.containsKey(label)) {
+                    labelMap.put(label, labelIndex++);
+                }
             }
         }catch (IOException e){
             e.printStackTrace();
         }
+        positiveLabel = labelMap.keySet().iterator().next();
+        for (String label : labelMap.keySet()) {
+            if (!label.equals(positiveLabel)) {
+                negativeLabel = label;
+                labelMap.put(label, 0);
+            }
+        }
+        labelMap.put(positiveLabel, 1);
+
+        return list;
     }
 
-    private void weigthInitializacion(){
-        for (int i = 0; i < weights.length; i++) {
+    private void initializeWeights(int size){
+        weights = new double[size];
+        for (int i = 0; i < size; i++) {
             weights[i] = Math.random() * 0.1 - 0.05;
         }
-        theta = Math.random() * 0.1 - 0.05;
+        bias = Math.random() * 0.1 - 0.05;
     }
 
-    private int netCalc(Observation observation){
+    private int activation(double net){
+        return net > 0 ? 1 : 0;
+    }
+
+    private double netCalc(Observation observation){
         double net = 0;
         for (int i = 0; i < observation.getFeatures().length; i++) {
-            net += observation.getFeatures()[i]*weights[i];
+            net += observation.getFeatures()[i] * weights[i];
         }
-        net += theta;
-        if (net > 0){
-            return 1;
-        }else{
-            return 0;
-        }
+        net += bias;
+        return net;
     }
-    public void train(){
-        for (int i = 0; i < observationList.size(); i++) {
-            int error = 0;
-            for (Observation observation : observationList){
 
+    public void train(List<Observation> observationList){
+        for (int i = 0; i < epoch; i++) {
+            for (Observation observation : observationList) {
+                double predicted = activation(netCalc(observation));
+                double trueLabel = labelMap.get(observation.getLabel());
+                double error = trueLabel - predicted;
+                for (int j = 0; j < observation.getFeatures().length; j++) {
+                    weights[j] += learningRate * error * observation.getFeatures()[j];
+                }
+                bias += learningRate * error;
             }
         }
     }
+
+    public String predict(Observation observation){
+        int prediction = (int) activation(netCalc(observation));
+        return prediction == 1 ? positiveLabel : negativeLabel;
+    }
+
 }
